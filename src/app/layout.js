002c50d8e095f38a056f9c3fccb99b35643059b9
NishaBE26 <nisha.26112k@gmail.com>
2025-04-30 17:53:41 +0530

    login page design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,17 @@
 import './globals.css';
-import './Styles/Sidebar.css';
-import SidebarWrapper from './components/SidebarWrapper';
-
 export const metadata = {
   title: 'News4Tamil',
   icons: {
     icon: '/favicon.ico',
   },
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="layout">
-          <SidebarWrapper />
           <main className="main-content">
             {children}
           </main>
-        </div>
       </body>
     </html>
   );
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,27 +2,37 @@
 import React from 'react';
 import "../Styles/login.css";
 import { useRouter } from 'next/navigation';
+
 const LoginPage = () => {
     const router = useRouter();
+
     const handleLogin = (event) => {
         event.preventDefault();
-        router.push('/posts'); 
+        router.push('/posts');
     };
+
     return (
-        <div className="login-page">
-        <form className="login-form" onSubmit={handleLogin}>
-            <h2 className='login-title'>Login</h2>
-            <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username"/>
+        <div className="login-wrapper">
+            <div className="login-page">
+                <div className="right-panel">
+                    <form className="login-form" onSubmit={handleLogin}>
+                        <h2 className='login-title'>Login</h2>
+                        <div className="inline-form">
+                            <div className="form-group">
+                                <label htmlFor="username">Username:</label>
+                                <input type="text" id="username" name="username" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password:</label>
+                                <input type="password" id="password" name="password" />
+                            </div>
+                            <button type="submit">Log in</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password"/>
-            </div>
-            <button type="submit">Log in</button>
-        </form>
         </div>
     );
-    }
+};
+
 export default LoginPage;
